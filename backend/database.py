4f8672ce1279e9a1defd3d932a13745403af6df7
Ayaan-Khan-18 <ayaansohailkhan786@gmail.com>
2026-04-20 15:19:36 +0530

    fixed bugs in db

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 import os
 
 load_dotenv()
 
+password = quote_plus(os.getenv('DB_PASSWORD'))  # Ayaan%40786
+
 DB_URL = (
-    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{password}"
     f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 )
 
